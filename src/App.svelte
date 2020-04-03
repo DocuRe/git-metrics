a10d8028ec05f78e17ad87ad087179e3f777d38e
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-04-03 12:29:55 -0400

    NEW: remove text from app

--- src/App.svelte
+++ src/App.svelte
@@ -3,23 +3,3 @@
 </script>
 
 <Router />
-
-<p>
-  Based on
-  <a
-    href="https://dev.to/richharris/a-new-technique-for-making-responsive-javascript-free-charts-gmp">
-    Svelte / Pancake by Rich Harris
-  </a>
-  .
-</p>
-<p>The below library reference looks promising for pulling data from github</p>
-<lu>
-  <li>
-    <a href="https://octokit.github.io/rest.js/v17#issues">List issues</a>
-  </li>
-  <li>
-    <a href="https://octokit.github.io/rest.js/v17#issues-list-for-repo">
-      List Repo Issues
-    </a>
-  </li>
-</lu>
