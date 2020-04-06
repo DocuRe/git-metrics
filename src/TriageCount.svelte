c4987ff59eed20797abf36b4cea078c6d2ddd3b7
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-04-06 16:27:11 -0400

    NEW: show column name and issue count per column

--- src/TriageCount.svelte
+++ src/TriageCount.svelte
@@ -2,14 +2,12 @@
   import * as Pancake from "@sveltejs/pancake";
   import Router from "./Router.svelte";
   import { getTriageProjectIssues } from "./data.js";
-  import config from '../config.json';
+  import config from "../config.json";
 
-  const organization = config.repositories[0].split('/')[0];
+  const organization = config.repositories[0].split("/")[0];
 
   const colors = {};
 
-  let maxCount = 0;
-  let totalCount = 0;
   let columns;
 
   $: loadIssues(organization);
@@ -17,14 +15,11 @@
   async function loadIssues(organization) {
     const projectColumns = await getTriageProjectIssues(organization);
 
-    maxCount = 0;
-    totalCount = 0;
-
     columns = projectColumns.map(({ name, cards }) => {
       const issues = cards.nodes;
       const groups = {};
-
-      totalCount += cards.totalCount;
+      const totalCount = cards.totalCount;
+      let maxCount = 0;
 
       for (let k = 0; k < issues.length; k++) {
         //this loops over the cards returned, each card is an issue
@@ -56,7 +51,7 @@
           return b.count - a.count;
         });
 
-      return { maxCount, counts };
+      return { maxCount, counts, name, totalCount };
     });
   }
 </script>
@@ -88,42 +83,45 @@
 </style>
 
 {#if columns}
-  <p style="font-size: 8pt">
-    This organization has a total of {totalCount} issues
-  </p>
   <div class="flex">
-    {#each columns as { maxCount, counts }}
-      <table>
-        <thead>
-          <tr>
-            <th>Label</th>
-            <th>
-              <div class="axis">
-                <Pancake.Chart x1={0} x2={maxCount / 0.9} y1={0} y2={0}>
-                  <Pancake.Grid vertical count={3} let:value>
-                    <span class="y label">{value}</span>
-                  </Pancake.Grid>
-                </Pancake.Chart>
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each counts as { repository, label, count }}
+    {#each columns as { name, maxCount, counts, totalCount }}
+      <div>
+        <p style="font-size: 8pt; text-align: center">
+          "{name}" has a total of {totalCount} issues
+        </p>
+        <table>
+          <thead>
             <tr>
-              <th style="font-size: 9pt">{label}</th>
-              <td style="font-size: 8pt">
-                <span
-                  class="bar"
-                  style="width: {(90 * count) / maxCount}%; background-color: #{colors[label]}"
-                  title="{count} issues" />
-                {count}
-                {#if count === 1}issue{:else}issues{/if}
-              </td>
+              <th>Label</th>
+              <th>
+                <div class="axis">
+                  <Pancake.Chart x1={0} x2={maxCount / 0.9} y1={0} y2={0}>
+                    <Pancake.Grid vertical count={3} let:value>
+                      <span class="y label">{value}</span>
+                    </Pancake.Grid>
+                  </Pancake.Chart>
+                </div>
+              </th>
             </tr>
-          {/each}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {#each counts as { repository, label, count }}
+              <tr>
+                <th style="font-size: 9pt">{label}</th>
+                <td style="font-size: 8pt">
+                  <span
+                    class="bar"
+                    style="width: {(90 * count) / maxCount}%; background-color:
+                    #{colors[label]}"
+                    title="{count} issues" />
+                  {count}
+                  {#if count === 1}issue{:else}issues{/if}
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
     {/each}
   </div>
 {:else}
