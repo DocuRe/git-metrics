51cb5181a89093a138181fa3192d074c986d7d64
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-08-21 13:19:41 -0400

    NEW: styling and GitHub link

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,69 @@
 <script>
-  import Router from './Router.svelte';
+	//import Flare from "./Flare.svelte";
+	import Router, { location } from 'svelte-spa-router';
+	import TriageCount from './TriageCount.svelte';
+	import TriageIssues from './TriageIssues.svelte';
+	import IssueAge from './IssueAge.svelte';
+	import IssueBacklog from './IssueBacklog.svelte';
+	import Owners from './Owners.svelte';
+
+	const routes = {
+		'/issue': IssueAge,
+		'/triage': TriageCount,
+		'/backlog': IssueBacklog,
+		'/triage-issues/:organization/:column/:label': TriageIssues,
+		'/owners': Owners,
+	};
 </script>
 
-<Router />
-<p>
-  Using 
-  <a
-    href="https://svelte.dev/">
-    Svelte
-  </a>
-  /
-  <a
-    href="https://dev.to/richharris/a-new-technique-for-making-responsive-javascript-free-charts-gmp">
-    Pancake 
-  </a>
-  by Rich Harris.
-</p>
+<style>
+	:global(body) {
+		padding: 0;
+	}
+
+	nav {
+		background: #336374;
+	}
+
+	a {
+		color: white;
+		margin: 0 1em;
+		display: inline-block;
+		padding: 0.5em;
+	}
+
+	.active {
+		background: rgba(255, 255, 255, 0.5);
+		color: #000;
+	}
+
+	main {
+		margin: 1em;
+	}
+
+	.github {
+		float: right;
+		padding: 0;
+	}
+
+	.github img {
+		display: block;
+	}
+</style>
+
+<nav>
+	<a href="#/issue" class:active={$location === '/issue'}>Issue Age</a>
+	<a href="#/backlog" class:active={$location === '/backlog'}>
+		Issue Backlog
+	</a>
+	<a href="#/triage" class:active={$location === '/triage'}>Triage Count</a>
+	<a href="#/owners" class:active={$location === '/owners'}>OWNERS Files</a>
+
+	<a class="github" href="https://github.com/docure/git-metrics">
+		<img width="85" height="35" src="GitHub_Logo_White.png" alt="GitHub" />
+	</a>
+</nav>
+
+<main>
+	<Router {routes} />
+</main>
